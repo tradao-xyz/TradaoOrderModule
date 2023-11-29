f18ec5eb310ab5b51bc990548ba96fe54a81eb53
Letho <3763819+LaceLetho@users.noreply.github.com>
2023-11-29 23:37:13 +0800

    feat: create order

--- src/Gmxv2OrderModule.sol
+++ src/Gmxv2OrderModule.sol
@@ -12,8 +12,11 @@ import "./interfaces/IDatastore.sol";
 import "./interfaces/Enum.sol";
 import "./interfaces/IModuleManager.sol";
 import "./interfaces/ISmartAccountFactory.sol";
+import "./interfaces/IWNT.sol";
+import "./interfaces/IExchangeRouter.sol";
 
-//Arbitrum configs
+//1. Arbitrum configs
+//2. Operator should approve WETH to this contract
 contract Gmxv2OrderModule is Ownable {
     address public operator;
     uint256 public ethPrice;
@@ -31,7 +34,6 @@ contract Gmxv2OrderModule is Ownable {
     address private constant REFERRALSTORAGE = 0xe6fab3F0c7199b0d34d7FbE83394fc0e0D06e99d;
     ISmartAccountFactory private constant BICONOMY_FACTORY =
         ISmartAccountFactory(0x000000a56Aaca3e9a4C479ea6b6CD0DbcB6634F5);
-    address private constant ENTRYPOINT_ADDRESS = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
     bytes private constant SETREFERRALCODECALLDATA =
         abi.encodeWithSignature("setTraderReferralCodeByUser(bytes32)", REFERRALCODE);
     bytes private constant MODULE_SETUP_DATA = abi.encodeWithSignature("getModuleAddress()");
@@ -43,12 +45,36 @@ contract Gmxv2OrderModule is Ownable {
     uint256 private ETH_MULTIPLIER = 10 ** 18;
     uint256 private USDC_MULTIPLIER = 10 ** 6;
     address private constant ORDER_VAULT = 0x31eF83a530Fde1B38EE9A18093A333D8Bbbc40D5;
+    IExchangeRouter private constant EXCHANGE_ROUTER = IExchangeRouter(0x7C68C7866A64FA2160F78EEaE12217FFbf871fa8);
 
     event OperatorTransferred(address indexed previousOperator, address indexed newOperator);
     event UpdateTokenPrice(address indexed token, uint256 newPrice);
     event NewSmartAccount(address indexed creator, address userEOA, address smartAccount);
+    event PayGasFailed(address indexed aa, uint256 indexed positionId);
+    event OrderCreated(
+        address indexed aa,
+        uint256 indexed positionId,
+        bytes32 orderKey,
+        uint256 sizeDelta,
+        uint256 collateralDelta,
+        uint256 acceptablePrice
+    );
+    event OrderCreationFailed(
+        address indexed aa,
+        uint256 indexed positionId,
+        uint256 sizeDelta,
+        uint256 collateralDelta,
+        uint256 acceptablePrice
+    );
 
     error UnsupportedOrderType();
+
+    struct OrderParam {
+        uint256 sizeDeltaUsd;
+        uint256 initialCollateralDeltaAmount;
+        uint256 acceptablePrice;
+        address smartAccount;
+    }
 
     /**
      * @dev Only allows addresses with the operator role to call the function.
@@ -72,15 +98,86 @@ contract Gmxv2OrderModule is Ownable {
     //single order, could contain trigger price
     function newOrder() external onlyOperator {}
 
-    //copy trading orders
-    function newOrders() external onlyOperator {}
+    /**
+     *   copy trading orders.
+     *   do off chain check before every call:
+     *   1. check if very aa's module is enabled
+     *   2. check aa's balance
+     *   3. get latest eth price, estimate gas
+     *   4. do simulation call
+     */
+    function newOrders(
+        uint256 positionId, //followee's positionId
+        uint256 _ethPrice,
+        uint256 _txGas,
+        address market,
+        Order.OrderType orderType,
+        bool isLong,
+        OrderParam[] memory orderParams
+    ) external onlyOperator {
+        (uint256 _txGasFee, uint256 _executionGasFee) = _calcGas(_ethPrice, orderType, _txGas);
+        uint256 len = orderParams.length;
+        bool isIncreaseOrder = BaseOrderUtils.isIncreaseOrder(orderType);
+        for (uint256 i; i < len; i++) {
+            OrderParam memory _orderParam = orderParams[i];
+            //todo use try catch to constrain the revert region
+            bool isSuccess = _payGas(_orderParam.smartAccount, _txGasFee, _executionGasFee, _ethPrice);
+            if (!isSuccess) {
+                emit PayGasFailed(_orderParam.smartAccount, positionId);
+                break;
+            }
+
+            if (isIncreaseOrder && _orderParam.initialCollateralDeltaAmount > 0) {
+                require(
+                    _aaTransferUsdc(_orderParam.smartAccount, _orderParam.initialCollateralDeltaAmount, ORDER_VAULT),
+                    "col"
+                );
+            }
+
+            //build orderParam
+            BaseOrderUtils.CreateOrderParams memory cop;
+            _buildOrderCommonPart(_executionGasFee, market, orderType, isLong, cop);
+            _buildOrderCustomPart(_orderParam, cop);
+
+            //send order
+            bytes32 orderKey = _aaCreateOrder(cop);
+            if (orderKey == 0) {
+                emit OrderCreationFailed(
+                    _orderParam.smartAccount,
+                    positionId,
+                    _orderParam.sizeDeltaUsd,
+                    _orderParam.initialCollateralDeltaAmount,
+                    _orderParam.acceptablePrice
+                );
+            } else {
+                emit OrderCreated(
+                    _orderParam.smartAccount,
+                    positionId,
+                    orderKey,
+                    _orderParam.sizeDeltaUsd,
+                    _orderParam.initialCollateralDeltaAmount,
+                    _orderParam.acceptablePrice
+                );
+            }
+        }
+    }
+
+    //return orderKey == 0 if failed.
+    function _aaCreateOrder(BaseOrderUtils.CreateOrderParams memory cop) internal returns (bytes32 orderKey) {
+        bytes memory data = abi.encodeWithSelector(EXCHANGE_ROUTER.createOrder.selector, cop);
+        (bool success, bytes memory returnData) = IModuleManager(cop.addresses.receiver)
+            .execTransactionFromModuleReturnData(address(EXCHANGE_ROUTER), 0, data, Enum.Operation.Call);
+        if (success) {
+            orderKey = bytes32(returnData);
+        }
+    }
 
     //cancel single order
     function cancelOrder(address smartAccount, bytes32 key) external onlyOperator {}
 
-    function _calcGasFee(uint256 _ethPrice, Order.OrderType orderType, uint256 txGas)
+    function _calcGas(uint256 _ethPrice, Order.OrderType orderType, uint256 _txGas)
         internal
-        returns (uint256 txGasFee, uint256 executionFee)
+        returns (uint256 txGasFee, uint256 executionGasFee)
     {
         require(_ethPrice * 100 < ethPrice * MAXPRICEBUFFERACTOR, "ethPrice");
         if (_ethPrice * 100 >= ethPrice * PRICEUPDATEACTOR) {
@@ -95,58 +192,66 @@ contract Gmxv2OrderModule is Ownable {
         } else {
             revert UnsupportedOrderType();
         }
-        require(txGas * 100 < executionFeeGasLimit * MAXTXGASRATIO, "txGas");
+        require(_txGas * 100 < executionFeeGasLimit * MAXTXGASRATIO, "txGas");
 
-        txGasFee = txGas * tx.gasprice;
-        executionFee = executionFeeGasLimit * tx.gasprice;
+        txGasFee = _txGas * tx.gasprice;
+        executionGasFee = executionFeeGasLimit * tx.gasprice;
+    }
+
+    function _payGas(address aa, uint256 txGasFee, uint256 executionFee, uint256 _ethPrice)
+        internal
+        returns (bool isSuccess)
+    {
+        if (aa.balance < txGasFee + executionFee) {
+            if (IERC20(WETH).balanceOf(operator) < executionFee) {
+                return false;
+            }
+            //transfer gas fee and execution fee USDC from AA to TinySwap
+            isSuccess = _aaTransferUsdc(aa, _calcUsdc(txGasFee + executionFee, _ethPrice), operator);
+        } else {
+            //convert ETH to WETH to operator
+            bytes memory data = abi.encodeWithSelector(IWNT(WETH).depositTo.selector, operator);
+            isSuccess =
+                IModuleManager(aa).execTransactionFromModule(WETH, txGasFee + executionFee, data, Enum.Operation.Call);
+        }
+        //transfer execution fee WETH from operator to GMX Vault
+        if (isSuccess) {
+            require(IERC20(WETH).transferFrom(operator, ORDER_VAULT, executionFee), "op eth");
+        }
     }
 
     function _buildOrderCommonPart(
-        BaseOrderUtils.CreateOrderParams memory params,
+        uint256 executionFee,
         address market,
         Order.OrderType orderType,
-        bool isLong
-    ) internal pure returns (BaseOrderUtils.CreateOrderParams memory) {
+        bool isLong,
+        BaseOrderUtils.CreateOrderParams memory params
+    ) internal pure {
+        params.numbers.executionFee = executionFee;
+
         params.addresses.market = market;
         params.orderType = orderType;
         params.isLong = isLong;
-        return params;
     }
 
-    function _buildOrderCustomPart(
-        BaseOrderUtils.CreateOrderParams memory params,
-        address smartAccount,
-        uint256 sizeDeltaUsd,
-        uint256 initialCollateralDeltaAmount,
-        uint256 acceptablePrice,
-        uint256 executionFee
-    ) internal view returns (BaseOrderUtils.CreateOrderParams memory) {
-        if (msg.value > 0) {
-            //1. transfer execution fee ETH from this contract to GMX Vault
-            //2. transfer gas fee and execution fee USDC from this to TinySwap
-        } else {
-            //1. transfer execution fee ETH from AA to GMX Vault
-            //2. transfer gas fee ETH from this to TinySwap
-        }
-
-        params.addresses.receiver = smartAccount;
+    function _buildOrderCustomPart(OrderParam memory _orderParam, BaseOrderUtils.CreateOrderParams memory params)
+        internal
+        view
+    {
+        params.addresses.receiver = _orderParam.smartAccount;
         params.addresses.callbackContract = address(0);
         params.addresses.uiFeeReceiver = operator;
-
         params.addresses.initialCollateralToken = USDC;
 
-        params.numbers.sizeDeltaUsd = sizeDeltaUsd;
-        params.numbers.initialCollateralDeltaAmount = initialCollateralDeltaAmount;
-        params.numbers.acceptablePrice = acceptablePrice;
-        params.numbers.executionFee = executionFee;
+        params.numbers.sizeDeltaUsd = _orderParam.sizeDeltaUsd;
+        params.numbers.initialCollateralDeltaAmount = _orderParam.initialCollateralDeltaAmount;
         params.numbers.callbackGasLimit = 0;
         params.numbers.minOutputAmount = 0;
+        params.numbers.acceptablePrice = _orderParam.acceptablePrice;
 
         params.decreasePositionSwapType = Order.DecreasePositionSwapType.SwapPnlTokenToCollateralToken;
         params.shouldUnwrapNativeToken = true;
         params.referralCode = REFERRALCODE;
-
-        return params;
     }
 
     function setReferralCode(address smartAccount) external {
@@ -160,16 +265,22 @@ contract Gmxv2OrderModule is Ownable {
         if (msg.sender == operator) {
             require(deployGas <= MAX_AA_DEPLOY_GAS, "gas");
             //transfer gas fee to TinySwap...
-            uint256 usdcAmount =
-                deployGas * tx.gasprice * ethPrice * USDC_MULTIPLIER / ETH_MULTIPLIER / ethPriceMultiplier;
-            _transferUsdcToOperator(aa, usdcAmount);
+            _aaTransferUsdc(aa, _calcUsdc(deployGas * tx.gasprice, ethPrice), operator);
         }
         emit NewSmartAccount(msg.sender, userEOA, aa);
     }
 
-    function _transferUsdcToOperator(address aa, uint256 amount) internal returns (bool isSuccess) {
-        bytes memory data = abi.encodeWithSelector(IERC20.transfer.selector, operator, amount);
+    function _aaTransferUsdc(address aa, uint256 usdcAmount, address to) internal returns (bool isSuccess) {
+        bytes memory data = abi.encodeWithSelector(IERC20.transfer.selector, to, usdcAmount);
         isSuccess = IModuleManager(aa).execTransactionFromModule(USDC, 0, data, Enum.Operation.Call);
+    }
+
+    function _calcUsdc(uint256 ethAmount, uint256 _ethPrice) internal view returns (uint256 usdcAmount) {
+        return ethAmount * _ethPrice * USDC_MULTIPLIER / ETH_MULTIPLIER / ethPriceMultiplier;
+    }
+
+    function _aaTransferEth(address aa, uint256 ethAmount, address to) internal returns (bool isSuccess) {
+        isSuccess = IModuleManager(aa).execTransactionFromModule(to, ethAmount, "", Enum.Operation.Call);
     }
 
     function _deployAA(address userEOA) internal returns (address) {
