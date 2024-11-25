002de976e1c719d4b1ada1ab8b6efc3f7f9a7566
Letho <3763819+LaceLetho@users.noreply.github.com>
2024-11-25 19:49:06 +0800

    doc: version

--- src/Gmxv2OrderModule.sol
+++ src/Gmxv2OrderModule.sol
@@ -21,7 +21,7 @@ import "./interfaces/IBiconomyModuleSetup.sol";
 import "./interfaces/IEcdsaOwnershipRegistryModule.sol";
 import "./interfaces/IPostExecutionHandler.sol";
 
-//v2.2.2
+//v2.3.0
 //Arbitrum equipped
 contract Gmxv2OrderModule is Initializable, OwnableUpgradeable, UUPSUpgradeable, IOrderCallbackReceiver {
     mapping(address => address) public operators;
